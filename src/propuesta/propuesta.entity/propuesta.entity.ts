c5533264959e445237c4fc46d3908fc0b597381c
Santiago <s.navarretev@uniandes.edu.co>
2024-11-27 20:37:37 -0500

    JoinTable

--- src/propuesta/propuesta.entity/propuesta.entity.ts
+++ src/propuesta/propuesta.entity/propuesta.entity.ts
@@ -3,12 +3,15 @@
 // import { BiteEntity } from '../../bite/bite.entity/bite.entity';
 // import { ProfesorEntity } from 'src/components/profesor/profesor.entity/profesor.entity';
 // import { ProyectoEntity } from 'src/components/proyecto/proyecto.entity/proyecto.entity';
-import { Column, Entity, OneToMany, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, JoinTable, OneToMany, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class PropuestaEntity {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+    // @PrimaryGeneratedColumn('uuid')
+    // id: string;
+
+    @PrimaryGeneratedColumn()
+    id: number;
 
     @Column()
     titulo: string;
@@ -20,8 +23,10 @@ export class PropuestaEntity {
     palabraClave: string;
 
     // @OneToOne(() => ProyectoEntity, proyecto => proyecto.propuesta)
+    //@JoinTable()
     // proyecto: ProyectoEntity;
 
     // @OneToMany(() => ProfesorEntity, profesor => profesor.propuesta)
+    //@JoinTable()
     // profesores: ProfesorEntity[];
 }
